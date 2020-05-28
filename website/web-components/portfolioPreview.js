ab8d08e003af2eab5669d9c722184dc80bdf31dd
Ezra Marks <43857686+EzraMarks@users.noreply.github.com>
2020-05-28 17:27:13 -0400

    Change indentation to 4 spaces

--- website/web-components/portfolioPreview.js
+++ website/web-components/portfolioPreview.js
@@ -1,115 +1,115 @@
 class PortfolioPreview extends HTMLElement {
 
-  constructor() {
-    super();
-    this.attachShadow({ mode: 'open' });
-  }
+    constructor() {
+        super();
+        this.attachShadow({ mode: 'open' });
+    }
 
-  set description(desc) {
-    this.shadowRoot.innerHTML = `
-      <style>
-      /* main div for a project preview item */
-      .project-preview {
-        text-align: center;
-        border-radius: 10px;
-        margin: 12px;
-        padding: 0px;
-        background-repeat: no-repeat;
-        background-size: 80%;
-        background-position: center;
-        background-color: ${desc.thumbnail.color};
-        background-image: url("${desc.thumbnail.img}");
-      }
+    set description(desc) {
+        this.shadowRoot.innerHTML = `
+            <style>
+            /* main div for a project preview item */
+            .project-preview {
+                text-align: center;
+                border-radius: 10px;
+                margin: 12px;
+                padding: 0px;
+                background-repeat: no-repeat;
+                background-size: 80%;
+                background-position: center;
+                background-color: ${desc.thumbnail.color};
+                background-image: url("${desc.thumbnail.img}");
+            }
 
-      /* inner link container which resides in each div,
-      * so the link will take up the entire div, excluding the margin/padding */
-      .link {
-        opacity: 0;
-        display: block;
-        width: 100%;
-        height: 100%;
-        color: white;
-        text-decoration: none;
-        position: relative;
-        overflow: hidden;
-        border-radius: inherit;
-        transition-property: opacity;
-        transition-duration: 0.2s;
-        background-color: ${desc.thumbnailHover.color || desc.thumbnail.color}
-      }
+            /* inner link container which resides in each div,
+            * so the link will take up the entire div, excluding the margin/padding */
+            .link {
+                opacity: 0;
+                display: block;
+                width: 100%;
+                height: 100%;
+                color: white;
+                text-decoration: none;
+                position: relative;
+                overflow: hidden;
+                border-radius: inherit;
+                transition-property: opacity;
+                transition-duration: 0.2s;
+                background-color: ${desc.thumbnailHover.color || desc.thumbnail.color}
+            }
 
-      /* on hover, show the contents of the container */
-      .link:hover {
-        opacity: 1;
-        transition-property: opacity;
-        transition-duration: 0.3s;
-      }
+            /* on hover, show the contents of the container */
+            .link:hover {
+                opacity: 1;
+                transition-property: opacity;
+                transition-duration: 0.3s;
+            }
 
-      /* position & style text in the on-hover preview */
-      .hover-text {
-        position: absolute;
-        width: 80%;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        z-index: 999;
-      }
+            /* position & style text in the on-hover preview */
+            .hover-text {
+                position: absolute;
+                width: 80%;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                z-index: 999;
+            }
 
-      /* position and style the graphic in the on-hover preview */
-      .hover-graphic * {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
-        opacity: 0.8;
-        border-radius: inherit;
-        width: 100%;
-      }
+            /* position and style the graphic in the on-hover preview */
+            .hover-graphic * {
+                position: absolute;
+                top: 50%;
+                left: 50%;
+                transform: translate(-50%, -50%);
+                opacity: 0.8;
+                border-radius: inherit;
+                width: 100%;
+            }
 
-      /* maintain a 16:9 aspect ratio on each item */
-      .aspect-ratio-box::before {
-        content: "";
-        width: 1px;
-        margin-left: -1px;
-        float: left;
-        height: 0;
-        padding-top: 56.25%; /* controls the aspect ratio */
-      }
-      .aspect-ratio-box::after {
-        content: "";
-        display: table;
-        clear: both;
-      }
-    </style>
+            /* maintain a 16:9 aspect ratio on each item */
+            .aspect-ratio-box::before {
+                content: "";
+                width: 1px;
+                margin-left: -1px;
+                float: left;
+                height: 0;
+                padding-top: 56.25%; /* controls the aspect ratio */
+            }
+            .aspect-ratio-box::after {
+                content: "";
+                display: table;
+                clear: both;
+            }
+        </style>
 
-    <div class="project-preview" title="">
-      <a class="link" href="${desc.portfolioLink}">
-        <div class="aspect-ratio-box">
-          <div class="hover-text">
-            <h2 class="title">${desc.title}</h2>
-            <h3 class="description">${desc.subtitle}</h3>
-          </div>
-          <div class="hover-graphic">
-            ${function generateHoverGraphicHtml() {
-              let hoverGraphic = ``;
-              if (desc.thumbnailHover.img != null) {
-                hoverGraphic = `<img src="${desc.thumbnailHover.img}"/>`
-              } else if (desc.thumbnailHover.video != null) {
-                hoverGraphic = `
-                  <video autoplay muted loop>
-                  <source src="${desc.thumbnailHover.video}"
-                  type="video/mp4"/>
-                  </video>
-                  `;
-              }
-              return hoverGraphic;
-            }()}
-          </div>
+        <div class="project-preview" title="">
+            <a class="link" href="${desc.portfolioLink}">
+                <div class="aspect-ratio-box">
+                    <div class="hover-text">
+                        <h2 class="title">${desc.title}</h2>
+                        <h3 class="description">${desc.subtitle}</h3>
+                    </div>
+                    <div class="hover-graphic">
+                        ${function generateHoverGraphicHtml() {
+                            let hoverGraphic = ``;
+                            if (desc.thumbnailHover.img != null) {
+                                hoverGraphic = `<img src="${desc.thumbnailHover.img}"/>`
+                            } else if (desc.thumbnailHover.video != null) {
+                                hoverGraphic = `
+                                    <video autoplay muted loop>
+                                    <source src="${desc.thumbnailHover.video}"
+                                    type="video/mp4"/>
+                                    </video>
+                                    `;
+                            }
+                            return hoverGraphic;
+                        }()}
+                    </div>
+                </div>
+            </a>
         </div>
-      </a>
-    </div>
-    `;
-  }
+        `;
+    }
 }
 
 window.customElements.define('portfolio-preview', PortfolioPreview);
--- website/web-components/siteHeader.js
+++ website/web-components/siteHeader.js
@@ -1,110 +1,110 @@
 const siteHeaderTemplate = document.createElement('template');
 siteHeaderTemplate.innerHTML = `
-  <style>
-    /* Style the header with a grey background and some padding */
-    .header {
-      padding-top: 9px;
-      padding-bottom: 1px;
-      background-color: white;
-      position: fixed;
-      top: 0; /* Position the navbar at the top of the page */
-      width: inherit;
-      max-width: inherit;
-      z-index: 9999999999;
-      box-shadow: -200px 0px 0px 0px white, 200px 0px 0px 0px white;
-    }
-    
-    .header:after{ /* define header shadow */
-      content: "";
-      position: absolute;
-      z-index: 9999999998;
-      top: 100%;
-      bottom: 0;
-      width: 120%;
-      height: 20px;
-      left: -10%;
-      right: -10%;
-      background: radial-gradient(ellipse at 50% -20%, rgba(00, 00, 00, 0.12), rgba(255, 255, 255, 0) 60%);
-      opacity: 0;
-      transition-duration: 0.2s;
-    }
-    
-    .header-shadow.header:after{ /* fade in header shadow */
-      opacity: 1;
-      transition-duration: 0.3s;
-    }
-    
-    /* Style the header links */
-    .header a {
-      float: left;
-      color: rgb(165, 165, 165);
-      text-align: center;
-      padding: 0px;
-      margin: 15px;
-      text-decoration: none;
-      font-size: 1em;
-      transition-property: color;
-      transition-duration: 0.15s;
-    }
-    
-    /* Style the logo link (notice that we set the same value of line-height and font-size to prevent the header to increase when the font gets bigger */
-    .header a.logo {
-      font-size: 1.5em;
-      color: inherit;
-      margin-left: 0px;
-    }
-    
-    /* Change the background color on mouse-over */
-    .header a:hover {
-      color: inherit;
-      transition-property: color;
-      transition-duration: 0.15s;
-    }
-    
-    /* Style the active/current link*/
-    .header a.active {
-      color: inherit;
-    }
-    
-    /* Float the link section to the right */
-    .header-right {
-      float: right;
-    }
-  </style>
+    <style>
+        /* Style the header with a grey background and some padding */
+        .header {
+            padding-top: 9px;
+            padding-bottom: 1px;
+            background-color: white;
+            position: fixed;
+            top: 0; /* Position the navbar at the top of the page */
+            width: inherit;
+            max-width: inherit;
+            z-index: 9999999999;
+            box-shadow: -200px 0px 0px 0px white, 200px 0px 0px 0px white;
+        }
+        
+        .header:after{ /* define header shadow */
+            content: "";
+            position: absolute;
+            z-index: 9999999998;
+            top: 100%;
+            bottom: 0;
+            width: 120%;
+            height: 20px;
+            left: -10%;
+            right: -10%;
+            background: radial-gradient(ellipse at 50% -20%, rgba(00, 00, 00, 0.12), rgba(255, 255, 255, 0) 60%);
+            opacity: 0;
+            transition-duration: 0.2s;
+        }
+        
+        .header-shadow.header:after{ /* fade in header shadow */
+            opacity: 1;
+            transition-duration: 0.3s;
+        }
+        
+        /* Style the header links */
+        .header a {
+            float: left;
+            color: rgb(165, 165, 165);
+            text-align: center;
+            padding: 0px;
+            margin: 15px;
+            text-decoration: none;
+            font-size: 1em;
+            transition-property: color;
+            transition-duration: 0.15s;
+        }
+        
+        /* Style the logo link (notice that we set the same value of line-height and font-size to prevent the header to increase when the font gets bigger */
+        .header a.logo {
+            font-size: 1.5em;
+            color: inherit;
+            margin-left: 0px;
+        }
+        
+        /* Change the background color on mouse-over */
+        .header a:hover {
+            color: inherit;
+            transition-property: color;
+            transition-duration: 0.15s;
+        }
+        
+        /* Style the active/current link*/
+        .header a.active {
+            color: inherit;
+        }
+        
+        /* Float the link section to the right */
+        .header-right {
+            float: right;
+        }
+    </style>
 
-  <div class="header">
-    <a href="index.html" class="logo">Ezra Marks</a>
-    <div class="header-right">
-      <a class="navbar-link" href="index.html">Work</a>
-      <a class="navbar-link" href="about.html">About</a>
-      <a class="navbar-link" href="resume.html">Resume</a>
+    <div class="header">
+        <a href="index.html" class="logo">Ezra Marks</a>
+        <div class="header-right">
+            <a class="navbar-link" href="index.html">Work</a>
+            <a class="navbar-link" href="about.html">About</a>
+            <a class="navbar-link" href="resume.html">Resume</a>
+        </div>
     </div>
-  </div>
 `;
 
 class SiteHeader extends HTMLElement {
-  constructor() {
-    super();
-    document.body.appendChild(siteHeaderTemplate.content.cloneNode(true));
+    constructor() {
+        super();
+        document.body.appendChild(siteHeaderTemplate.content.cloneNode(true));
 
-    const activePage = this.getAttribute('active-page');
-    if (activePage != null) {
-      document.body.querySelector(`[class='navbar-link'][href='${activePage}']`).classList.add('active')
+        const activePage = this.getAttribute('active-page');
+        if (activePage != null) {
+            document.body.querySelector(`[class='navbar-link'][href='${activePage}']`).classList.add('active')
+        }
     }
-  }
 
-  connectedCallback() {
-    const header = document.querySelector('.header');
-    const sticky = header.offsetTop;
+    connectedCallback() {
+        const header = document.querySelector('.header');
+        const sticky = header.offsetTop;
 
-    window.onscroll = (event) => {
-      if (window.pageYOffset > sticky + 100) {
-        header.classList.add('header-shadow');
-      } else {
-        header.classList.remove('header-shadow');
-      }
+        window.onscroll = (event) => {
+            if (window.pageYOffset > sticky + 100) {
+                header.classList.add('header-shadow');
+            } else {
+                header.classList.remove('header-shadow');
+            }
+        }
     }
-  }
 }
 
 window.customElements.define('site-header', SiteHeader);
