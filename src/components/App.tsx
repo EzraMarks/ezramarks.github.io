84c7ff15c93fe9026a0bcf01b92d39bd4c8fa2c0
Ezra Marks <ezrablankmarks@gmail.com>
2021-07-23 21:05:42 -0400

    Continue configuring React router

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,16 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Home } from "./Home";
+import { Resume } from "./Resume";
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <Route path="/" component={Home} />
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/resume" exact component={Resume} />
+        <Route path="/" render={() => <div>404</div>} />
+      </Switch>
     </BrowserRouter>
   );
 };
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { Link, RouteComponentProps } from "react-router-dom";
 
-interface Props {}
+interface Props extends RouteComponentProps {}
 
-export const Home: React.FC<Props> = () => {
-  return <div>Home</div>;
+export const Home: React.FC<Props> = ({ history }) => {
+  const button = <Link to="/resume">Go to Resume page</Link>;
+  const button2 = (
+    <button
+      onClick={() => {
+        history.push("/about");
+      }}
+    ></button>
+  );
+
+  return (
+    <>
+      <div>Home</div>
+      {button}
+      {button2}
+    </>
+  );
 };
