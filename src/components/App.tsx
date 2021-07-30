87906eee184b1f66bbe430c2de0bb85030ad296b
Ezra Marks <ezrablankmarks@gmail.com>
2021-07-30 17:55:41 -0400

    Add resume page

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,17 @@
 import React from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import { HomePage } from "./HomePage/HomePage";
 import { ResumePage } from "./ResumePage";
 
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Switch>
         <Route path="/" exact component={HomePage} />
         <Route path="/resume" exact component={ResumePage} />
         <Route path="/" render={() => <div>404</div>} />
       </Switch>
-    </BrowserRouter>
+    </HashRouter>
   );
 };
 
--- src/components/ResumePage.tsx
+++ src/components/ResumePage.tsx
@@ -1,7 +1,23 @@
 import React from "react";
+import "./ResumePage.css";
+import { SiteHeader } from "./SiteHeader";
+import { RouteComponentProps } from "react-router";
 
-interface Props {}
+interface Props extends RouteComponentProps {}
 
-export const ResumePage: React.FC<Props> = () => {
-  return <div>Resume</div>;
+export const ResumePage: React.FC<Props> = (props) => {
+  return (
+    <>
+      <SiteHeader {...props} />
+      <div className="resume-page">
+        <div className="resume-pdf-container">
+          <iframe
+            className="resume-pdf"
+            src="./ezra-marks-resume.pdf#view=FitH"
+            title="Ezra Marks Resume"
+          ></iframe>
+        </div>
+      </div>
+    </>
+  );
 };
