f8ea5debe9b4fbdcf41825670cef7dea1d8fbfa1
Ezra Marks <ezrablankmarks@gmail.com>
2021-08-02 21:08:42 -0400

    Update project descriptions

--- src/ProjectDescriptions.tsx
+++ src/ProjectDescriptions.tsx
@@ -47,49 +47,9 @@ export const PROJECT_DESCRIPTIONS: { [name: string]: ProjectDescription } = {
       "https://www.youtube.com/watch?v=BiV0ookFeFs&ab_channel=EzraMarks",
     githubLink: "https://github.com/EzraMarks/reaction-diffusion",
   },
-  rhythmWitch: {
-    title: "Rhythm Witch",
-    subtitle: "2D Music Game",
-    body: (
-      <>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non
-          rhoncus purus, id tempor nisi. Quisque eu cursus risus, eget pharetra
-          eros. Phasellus ut nibh et neque vulputate volutpat. Nam purus lorem,
-          tristique ac faucibus ut, rutrum gravida massa. Suspendisse finibus at
-          risus iaculis condimentum. Suspendisse mollis nec mi sed fringilla.
-          Duis rhoncus magna turpis.
-        </p>
-        <p>Technologies: Stuff</p>{" "}
-      </>
-    ),
-    imageSrc: rhythmWitchImg,
-    demoLink: "https://brownrisdgames.itch.io/rhythmwitch",
-    githubLink: "https://github.com/EzraMarks/fractal-forest",
-  },
-  fractalForest: {
-    title: "Fractal Forest",
-    subtitle: "Interactive Art with JavaScript",
-    body: (
-      <>
-        <p>
-          Creating a fractal tree was one of my earliest coding projects; its
-          simple elegance helped inspire me to study Computer Science. Staying
-          home in Vermont during the Covid lockdown has given me some time to
-          revisit these beautiful, algorithmic patterns of nature, growing that
-          basic recursive fractal tree into an object-oriented Fractal Forest.
-          This project was built in JavaScript with the help of the p5.js
-          graphics library, created by the community behind the open-source{" "}
-          <a href="https://processing.org/">Processing</a> software.
-        </p>
-      </>
-    ),
-    imageSrc: fractalForestImg,
-    githubLink: "https://github.com/EzraMarks/fractal-forest",
-  },
-  epilepsy: {
+  strobeBlocker: {
     title: "Strobe Blocker",
-    subtitle: "Automated Removal of Photosensitive Epilepsy Triggers in Video",
+    subtitle: "Automated Removal of Seizure Triggers in Video",
     body: (
       <>
         <p>
@@ -109,5 +69,46 @@ export const PROJECT_DESCRIPTIONS: { [name: string]: ProjectDescription } = {
     ),
     imageSrc: pokemonShockGif,
     githubLink: "https://github.com/EzraMarks/epilepsy-video-augmentation",
+  },
+  fractalForest: {
+    title: "Fractal Forest",
+    subtitle: "Interactive Art with JavaScript",
+    body: (
+      <>
+        <p>
+          Creating a fractal tree was one of my earliest coding projects; its
+          simple elegance helped inspire me to study Computer Science. Staying
+          home in Vermont during the Covid lockdown has given me some time to
+          revisit these beautiful, algorithmic patterns of nature, growing that
+          basic recursive fractal tree into an object-oriented Fractal Forest.
+          This project was built in JavaScript with the help of the p5.js
+          graphics library, created by the community behind the open-source{" "}
+          <a href="https://processing.org/">Processing</a> software.
+        </p>
+      </>
+    ),
+    imageSrc: fractalForestImg,
+    demoLink: "https://www.ezramarks.com/fractal-forest",
+    githubLink: "https://github.com/EzraMarks/fractal-forest",
+  },
+  rhythmWitch: {
+    title: "Rhythm Witch",
+    subtitle: "2D Music Game",
+    body: (
+      <>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam non
+          rhoncus purus, id tempor nisi. Quisque eu cursus risus, eget pharetra
+          eros. Phasellus ut nibh et neque vulputate volutpat. Nam purus lorem,
+          tristique ac faucibus ut, rutrum gravida massa. Suspendisse finibus at
+          risus iaculis condimentum. Suspendisse mollis nec mi sed fringilla.
+          Duis rhoncus magna turpis.
+        </p>
+        <p>Technologies: Stuff</p>{" "}
+      </>
+    ),
+    imageSrc: rhythmWitchImg,
+    demoLink: "https://ezramarks.com/rhythm-witch",
+    githubLink: "https://github.com/EzraMarks/fractal-forest",
   },
 };
